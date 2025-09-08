15ff90291c52f04451f6970641732bb090174a11
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-09-08 19:06:38 +0530

    fix:CROS

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@
 
  app.use(express.json());
  app.use(cors({
-     origin:["http://localhost:5173" ,"https://second-brain-eosin.vercel.app/"],
+     origin:["http://localhost:5173" ,"https://second-brain-eosin.vercel.app"],
     allowedHeaders:['Content-Type','Authorization'],
     methods:['GET','POST','DELETE','OPTIONS'],
     credentials:true
